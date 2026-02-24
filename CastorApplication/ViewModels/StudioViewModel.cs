2036deddfb9488ba468e0b661e4ffe30131ec3fc
Baptiste Rachez <baptiste.rachez@epitech.eu>
2026-02-24 12:57:31 +0100

    feat(studio): Working raw docking system, not persisting + not tuned

--- CastorApplication/ViewModels/StudioViewModel.cs
+++ CastorApplication/ViewModels/StudioViewModel.cs
@@ -1,6 +1,12 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Dock.Model.Controls;
+using Dock.Model.Core;
 
-namespace CastorApplication.ViewModels
+namespace CastorApplication.ViewModels;
+
+public partial class StudioViewModel : ViewModelBase
 {
-    public partial class StudioViewModel : ViewModelBase { }
+    // C'est ici que le Layout sera stocké
+    [ObservableProperty]
+    private IRootDock? _layout;
 }
