f322558d9c3bdad888c6c5c991329580f69f6eef
Baptiste Rachez <baptiste.rachez@epitech.eu>
2026-02-24 12:50:41 +0100

    fix: correct project folder structure and file locations

---
+++ CastorApplication/Views/StudioView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace CastorApplication.Views;
+
+public partial class StudioView : UserControl
+{
+    public StudioView()
+    {
+        InitializeComponent();
+    }
+}
