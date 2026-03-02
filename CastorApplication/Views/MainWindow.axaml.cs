ea1d38a4ce85347e576aafc509cce95759c62923
deedax <andre.stana@epitech.eu>
2026-03-02 17:59:31 +0000

    Suppression de commentaires inutiles et nettoyage du code dans MainWindow pour améliorer la lisibilité.

--- CastorApplication/Views/MainWindow.axaml.cs
+++ CastorApplication/Views/MainWindow.axaml.cs
@@ -1,6 +1,5 @@
 using Avalonia.Controls;
 using CastorApplication.ViewModels;
-using System.ComponentModel;
 
 namespace CastorApplication.Views;
 
@@ -11,20 +10,11 @@ public partial class MainWindow : Window
         InitializeComponent();
     }
 
-    // Cette méthode est appelée automatiquement quand on ferme la fenêtre
     protected override void OnClosing(WindowClosingEventArgs e)
     {
-        // On récupère le ViewModel de la fenêtre
         if (DataContext is MainWindowViewModel mwvm)
         {
-            // On demande au StudioViewModel de sauvegarder son layout
-            // (Note: Assure-toi que ShowStudio a été appelé au moins une fois)
-            mwvm.ShowStudioCommand.Execute(null); // Force l'accès au VM si besoin
-
-            // Si tu as bien exposé ton champ _studioPage ou une propriété StudioPage
-            // On appelle la sauvegarde :
-            // mwvm.StudioPage?.SaveLayout(); 
-            // Note : Adapte selon le nom de la propriété que tu as créée pour le Studio
+            mwvm.StudioPage.SaveLayout();
         }
         base.OnClosing(e);
     }
